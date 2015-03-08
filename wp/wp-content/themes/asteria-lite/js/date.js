ba3930119bfcfd1780f1c425f4e0bad5a8f7dd8e
Zach Steiner <zsteiner@gmail.com>
2015-03-08 18:16:10 -0400

    Handcuffed Site update

--- wp/wp-content/themes/asteria-lite/js/date.js
+++
@@ -1,2 +0,0 @@
-// JavaScript Document
-jQuery(window).ready(function() {var date = jQuery('.ast_date').datepicker({ dateFormat: 'dd MM yy' }).val();});
--- wp/wp-content/themes/asteria-lite/tag.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<?php global $asteria;?>
-<!--Tag Posts-->
-<div class="fixed_site">
-	<div class="fixed_wrap">
-		<?php get_template_part('layout'.$asteria['cat_layout_id'].''); ?>
-	</div>
-</div>
-<?php get_footer(); ?>
